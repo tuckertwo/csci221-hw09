18c6c27f508a5d10de8b493f6f2ad621be8b0f24
Tucker R. Twomey <tucker@the-twomeys.com>
2022-04-08 15:18:15 -0700

    Write testcases for `check_permutation()`

--- test_cities.cc
+++ test_cities.cc
@@ -1,3 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hh"
 #include "cities.hh"
+
+// These test cases were based on the specification given in `tree.hh`.
+TEST_CASE("check_permutation")
+{
+  REQUIRE(!check_permutation({1,2,3,4}));
+  REQUIRE(check_permutation({0,1,2,3,4}));
+  REQUIRE(check_permutation({}));
+  REQUIRE(check_permutation({0}));
+  REQUIRE(!check_permutation({0}));
+  REQUIRE(!check_permutation({0,1,1,2,3,4}));
+  REQUIRE(!check_permutation({0,1,3,4}));
+}
