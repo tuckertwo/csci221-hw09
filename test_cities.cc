868e84b0f35c2affe4b3d3a9a5310aee8e3659b3
Tucker R. Twomey <tucker@the-twomeys.com>
2022-04-11 02:32:09 -0700

    Add testcases

--- test_cities.cc
+++ test_cities.cc
@@ -21,3 +21,16 @@ TEST_CASE("random_permutation")
   REQUIRE(random_permutation(1) == perm1);
   REQUIRE(check_permutation(random_permutation(128)));
 }
+
+TEST_CASE("Cities and other methods")
+{
+  Cities cities0;
+  // Pythagorean triples, to make distances nice non-floats.
+  Cities cities1({(0,0),  (3,4), (8,16), (16,31), (23,55), (14,15), (6,9),
+      (3,5), (3,4)});
+  // Evil irrational numbers
+  Cities cities2({(1,2),  (3,4),  (5,6), (7,8)});
+  REQUIRE(cities1.total_path_distance({0,1,2,3,4,5,6,7,8}) == 122);
+  REQUIRE(cities1.reorder({8,7,6,5,4,3,2,1,0}).total_path_distance({0,1,2,3,4,
+        5,6,7,8}) == 122);
+}
