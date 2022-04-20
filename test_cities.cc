e62dd66e918cbd9132442b271c82924bf03ec5c7
Tucker R. Twomey <tucker@the-twomeys.com>
2022-04-20 14:33:46 -0700

    Write testcases for I/O

--- test_cities.cc
+++ test_cities.cc
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hh"
 #include "cities.hh"
+#include <sstream>
 
 // These test cases were based on the specification given in `tree.hh`.
 TEST_CASE("check_permutation")
@@ -51,4 +52,55 @@ TEST_CASE("Cities and other methods")
   // irrational number.
   REQUIRE(tpd_ir>16);
   REQUIRE(tpd_ir<17);
+}
+
+TEST_CASE("I/O")
+{
+  // Somewhat pathological input case
+  const std::string pyth_inp
+    = "0 0\n3 4\n8 16\n16 31 \n\r\n23 55\n14\t15\n6 9\n3 5\n3 4\n";
+  // Correct output case
+  const std::string pyth_out
+    = "0\t0\n3\t4\n8\t16\n16\t31\n23\t55\n14\t15\n6\t9\n3\t5\n3\t4\n";
+
+  // These std::istringstreams let us simulate input from files.
+  std::istringstream test_inp_blank("");
+  std::istringstream test_inp_spc(" ");
+  std::istringstream test_inp_pyth(pyth_inp);
+
+  // Initialize blank Cities objects.
+  Cities blank;
+  Cities spc;
+  Cities pyth;
+  // Expected parse of PYTH_INP
+  Cities realpyth({std::make_pair(0,0),  std::make_pair(3,4),
+      std::make_pair(8,16),  std::make_pair(16,31), std::make_pair(23,55),
+      std::make_pair(14,15), std::make_pair(6,9),   std::make_pair(3,5),
+      std::make_pair(3,4)});
+
+  // Read inputs
+  test_inp_blank>>blank;
+  test_inp_spc>>spc;
+  test_inp_pyth>>pyth;
+
+  // Check that input reading works
+  REQUIRE(blank.total_path_distance({})==0);
+  REQUIRE(spc.total_path_distance({})==0);
+  REQUIRE(pyth.total_path_distance({0,1,2,3,4,5,6,7,8})
+      == realpyth.total_path_distance({0,1,2,3,4,5,6,7,8}));
+
+  // Analogous to std::istringstreams, these std::ostringstreams let us simulate
+  // output to a file.
+  std::ostringstream test_out_blank;
+  std::ostringstream test_out_pyth0;
+  std::ostringstream test_out_pyth1;
+
+  // Output Cities objects to our ostringstreams.
+  test_out_blank<<blank;
+  test_out_pyth0<<pyth;
+  test_out_pyth1<<realpyth;
+
+  REQUIRE(test_out_blank.str() == "");
+  REQUIRE(test_out_pyth0.str() == pyth_out);
+  REQUIRE(test_out_pyth1.str() == pyth_out);
 }
