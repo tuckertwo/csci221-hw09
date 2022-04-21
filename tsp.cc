2496317c90633ab0004e3af2a57add58c3969edd
Tucker R. Twomey <tucker@the-twomeys.com>
2022-04-21 10:05:32 -0700

    Make first attempt at writing `tsp` while offline

--- tsp.cc
+++ tsp.cc
@@ -3,7 +3,44 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-  cout << "foo\n";
+  Cities cities_obj;
+  if(argc>=2)
+  {
+    cerr<<"Incorrect number of arguments"<<endl;
+    return 1;
+  }
+  else if(argc==2)
+  {
+    //
+  }
+  else
+  {
+    cin>>cities_obj;
+  }
+
+  Cities::permutation_t perm = random_permutation(cities_obj.size());
+  unsigned target = cities_obj.total_path_distance(perm);
+  for(unsigned i_; i_<(2^cities_obj.size()); i_++) // Change me.
+  {
+    Cities::permutation_t attempt = random_permutation(cities_obj.size());
+    unsigned attempt_size         = cities_obj.total_path_distance(attempt);
+    if(attempt_size>target)
+    {
+      perm   = attempt;
+      target = attempt_size;
+    }
+  }
+
+  if(argc==2)
+  {
+    //
+  }
+  else
+  {
+    cout<<cities_obj.reorder(perm);
+  }
+  cerr<<"Done. Minimum distance: "<<target<<endl;
+  return 0;
 }
